ee349490cf77437915f35d8109370abc20f01da7
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-04-26 03:44:10 +0200

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -8,34 +8,26 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
-{
-	int curr_i = *i + 1;
-	int precision = -1;
+int get_precision(char *format, int *i, int *list) {
+  int precision = 0;
+  int num_digits;
 
-	if (format[curr_i] != '.')
-		return (precision);
+  // Calculate the number of digits after the decimal point in the formatted string.
+  for (num_digits = 0; format[num_digits] != '\0'; num_digits++) {
+    if (format[num_digits] == '.') {
+      break;
+    }
+  }
 
-	precision = 0;
+  // Iterate through the list of arguments, and for each argument, calculate the number of digits after the decimal point.
+  for (int j = 0; j < *i; j++) {
+    num_digits = 0;
+    while (list[j] != '\0' && isdigit(list[j])) {
+      num_digits++;
+      j++;
+    }
+    precision = max(precision, num_digits);
+  }
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+  return precision;
 }
